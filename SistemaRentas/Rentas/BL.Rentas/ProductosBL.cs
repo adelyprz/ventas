be1e87389f6b4d23d39bb00f284ff2a683864fde
Adely Perez <adelyalvla@gmail.com>
2021-06-25 06:08:52 -0600

    Agregando segundo avance ProductosBL

---
+++ SistemaRentas/Rentas/BL.Rentas/ProductosBL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Rentas
+{
+    public class ProductosBL
+    {
+        public BindingList<Producto> ListaProductos { get; set; }
+
+        public ProductosBL()
+        {
+            //Datos de prueba
+            ListaProductos = new BindingList<Producto>();
+
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "PS4 Slim";
+            producto1.Precio = 17000;
+            producto1.Existencia = 5;
+            producto1.Activo = true;
+
+            ListaProductos.Add(producto1); //Agrega la información del nuevo producto a la lista.
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "PS3";
+            producto2.Precio = 6000;
+            producto2.Existencia = 7;
+            producto2.Activo = true;
+
+            ListaProductos.Add(producto2);
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Mando PS4";
+            producto3.Precio = 1800;
+            producto3.Existencia = 8;
+            producto3.Activo = true;
+
+            ListaProductos.Add(producto3);
+        }
+
+        public BindingList<Producto> ObtenerProductos()
+        {
+            return ListaProductos;
+        }
+
+    }
+
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Existencia { get; set; }
+        public bool Activo { get; set; }
+    }
+}
--- SistemaRentas/Rentas/Win.Rentas/Form_Productos.cs
+++ SistemaRentas/Rentas/Win.Rentas/Form_Productos.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BL.Rentas;
 
 namespace Win.Rentas
 {
     public partial class Form_Productos : Form
     {
+        ProductosBL _productos;
+
         public Form_Productos()
         {
             InitializeComponent();
+
+            _productos = new ProductosBL();
+            listaProductosBindingSource.DataSource = _productos.ObtenerProductos();
         }
     }
 }
